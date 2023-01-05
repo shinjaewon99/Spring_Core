e579491ebc08920ef576ab1a2be20b6e87bf81e7
shinjaewon99 <tlswodnjs99@naver.com>
2023-01-05 23:36:04 +0900

    feat : Order 기능 구현 및 테스트

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,33 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        // 객체생성과 인터페이스에 어떤 객체가 할당되야되는지 만든다.
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        Long memberId = 1L;
+        // Grade를 VIP로 지정해 FixDiscount가 적용됨
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+
+
+    }
+}
---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,36 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+    private MemberRepository memberRepository;
+    
+    // 고정 할인
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+/*
+     OCP , DIP의 원칙을 지킨것 같지만 아니다.
+     이유 : new RateDiscountPolicy()가 구현 객체에도 의존하고 있음으로
+*/
+
+    // Grade 등급에 따라 할인이 다름
+    private DiscountPolicy discountPolicy;
+
+
+    // FixDiscount인지 , RateDiscount인지 외부(AppConfig)에서 주입된다.
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        // new 연산자를 사용해 Order 클래스의 값을 넣어 반환한다.
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
